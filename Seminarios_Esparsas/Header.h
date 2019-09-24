70833c8dcd4a20e5899677359a7f66ab422e9474
Kennedy <Kennedy@CHOLESKY>
2019-09-24 17:04:22 -0300

    amém

--- Seminarios_Esparsas/Header.h
+++ Seminarios_Esparsas/Header.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
+#include <fstream>
 
 using namespace std;
 
@@ -38,6 +39,27 @@ struct vertice{
 };
 
 
+vector<arco> ler_instancia(const char * filename) {
+	ifstream instancia(filename, ifstream::in);
+	if (instancia.fail()) {
+		cerr << "     Arquivo \"" << filename << "\" nao encontrado." << endl;
+		exit(0);
+	}
+	int n, tau;
+	instancia >> n >> tau;
+
+	vector<arco> G;
+	for (int it = 0; it < tau; it++){
+		int i, j; float v;
+
+		instancia >> i >> j >> v;
+
+		G.push_back(arco(i, j, v));
+
+	}
+	return G;
+}
+
 void imprimir_matriz(vector<arco> G, int n) {
 	vector < vector<bool>> MATRIZ(n);
 	for (int i = 0; i < n; i++)
@@ -48,13 +70,14 @@ void imprimir_matriz(vector<arco> G, int n) {
 		MATRIZ[arco.j][arco.i] = true;
 	}
 
-	cout << "_|_";
-	for (int i = 0; i < n; i++)
-		cout << i << "_";
-	cout << "|" << endl;
+	//cout << "_|_";
+	//for (int i = 0; i < n; i++)
+	//	cout << i << "_";
+	//cout << "|" << endl;
 
 	for (int i = 0; i < n; i++){
-		cout << i << "| ";
+		//cout << i << "| ";
+		cout << "| ";
 		for (int j = 0; j < n; j++){
 			if (MATRIZ[i][j] || (i == j))
 				cout << "x ";
@@ -67,12 +90,13 @@ void imprimir_matriz(vector<arco> G, int n) {
 
 
 //Assuma que a o grafo G é conexo e que a matriz é irredutível
+//retornando o reverso
 vector<arco> Cuthill_Mckee(vector<arco> G, int n) {
-
+	//declarar vetor auxiliar de graus e conjuntos de adjacência
 	vector<int> Graus(n, 0);
 	vector < vector<int>> adj(n);
 
-
+	//percorrer o grafo para atualizar graus e conjuntos de adjacência
 	for (auto a: G){
 		Graus[a.i]++;
 		Graus[a.j]++;
@@ -81,6 +105,7 @@ vector<arco> Cuthill_Mckee(vector<arco> G, int n) {
 		adj[a.j].push_back(a.i);
 	}
 
+	//jogar dados na estrutura de vértice
 	vector<vertice> Vert;
 	for (int i = 0; i < n; i++) {
 		vertice v = vertice(i, Graus[i]);
@@ -88,38 +113,61 @@ vector<arco> Cuthill_Mckee(vector<arco> G, int n) {
 		Vert.push_back(v);
 	}
 
-	sort(Vert.begin(), Vert.end());
+	//para cada vértice adicionar adjacentes por ordem de grau
 
+	//Montar S1
 	vector<int> permutacao;
+	vertice &min = *min_element(Vert.begin(), Vert.end());
+	min.visitado = true;
+	permutacao.push_back(min.label);
 
-	for (int i = 0; i < n; i++){
-		if (Vert[i].visitado == false) {
-
-			permutacao.push_back(Vert[i].label);
-			Vert[i].visitado = true;
-
-			for (int j = i + 1; j < n; j++) {
-				bool found = ( find(Vert[i].adj.begin(), Vert[i].adj.end(), Vert[j].label) != Vert[i].adj.end() );
-
-				if (found && Vert[j].visitado == false) {
-					permutacao.push_back(Vert[j].label);
-					Vert[j].visitado = true;
-				}
-			}
-		}
-		else
-			continue;
+	//Montar S2
+	vector<vertice> aux;
+	for (auto v : Vert[min.label].adj) {
+		aux.push_back(Vert[v]);
+		Vert[v].visitado = true;
+	}
+	sort(aux.begin(), aux.end());
+	for (auto a: aux){
+		permutacao.push_back(a.label);
 	}
 
-	vector<int> y(n, 0);
+	aux.clear();
 
+	for (int i = 1; i < n; i++){
+
+		//percorrer seus filhos e adicioná-los em ordem de grau caso não tenham sido visitados
+		for (auto filho: Vert[permutacao[i]].adj) {
+			//se for filho e ainda não foi visitado
+			if (Vert[filho].visitado == false) {
+				aux.push_back(Vert[filho]);
+				Vert[filho].visitado = true;
+			}
+		}
+		sort(aux.begin(), aux.end());
+		for (auto a : aux) {
+			permutacao.push_back(a.label);
+		}
+
+		aux.clear();
+	}
+
+	//invertendo a ordem
+	vector<int> y(n, 0);
 	for (int i = 0; i < n; i++)
 		y[i] = permutacao[n - 1 - i];
 
+	//criando novo grafo
+
+	vector<int> transformar(n);
+	for (int i = 0; i < n; i++)
+		transformar[y[i]] = i;
+
 	vector<arco> G_new;
 	for (auto a: G){
-		G_new.push_back(arco(y[a.i], y[a.j], a.v));
+		G_new.push_back(arco(transformar[a.i], transformar[a.j], a.v));
 	}
+
 
 	return G_new;
 }
--- Seminarios_Esparsas/Source.cpp
+++ Seminarios_Esparsas/Source.cpp
@@ -1,21 +1,17 @@
 #include "Header.h"
 
 void main() {
-	vector<vector<int>> MATRIZ = { { 0, 1, 1 },{ 1, 2, 1 },{ 1, 3, 1 },{ 1, 4, 1 },{ 1, 5, 1 },{ 1, 6, 1 } };
+	//vector<vector<int>> MATRIZ = { { 0, 1, 1 },{ 1, 2, 1 },{ 1, 3, 1 },{ 1, 4, 1 },{ 1, 5, 1 },{ 1, 6, 1 } };
 
-	int n = 7,
-		tau = 6;
-
-	vector<arco> G;
-	for (int i = 0; i < tau; i++)
-		G.push_back(arco(MATRIZ[i][0], MATRIZ[i][1], MATRIZ[i][2]));
+	int n = 15;
+	vector<arco> G = ler_instancia("matrix_exemplo_15.txt");
 
 
-	imprimir_matriz(G, n, tau);
+	imprimir_matriz(G, n);
 
 	cout << endl << endl;
 
-	imprimir_matriz(Cuthill_Mckee(G, n, tau), n, tau);
+	imprimir_matriz(Cuthill_Mckee(G, n), n);
 
 	cout << "Hello World!" << endl;
 
